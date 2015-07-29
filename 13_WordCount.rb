cb983dff4c2dccd60ee29c3d8e07afe8aeb67c2b
hello15369 <junejc0613@naver.com>
2015-07-29 14:56:27 +0900

    WordCount
    WordCount

--- 13_WordCount.rb
+++ 13_WordCount.rb
@@ -4,7 +4,7 @@ def WordCount(str)
   return str.length
          
 end
-   
+
 # keep this function call here 
 # to see how to enter arguments in Ruby scroll down   
 WordCount(STDIN.gets)           
