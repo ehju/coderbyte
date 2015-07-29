481f6a8d295ef9d897fbb732614aa37ed83826b3
hello15369 <junejc0613@naver.com>
2015-07-29 15:21:18 +0900

    AlphabetSoup
    Alphabet 순서대로 정렬

---
+++ 10_AlphabetSoup.rb
@@ -0,0 +1,12 @@
+def AlphabetSoup(str)
+  str=str.split""
+  str= str.sort
+
+  # code goes here
+  return str.join
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+AlphabetSoup(STDIN.gets)           
